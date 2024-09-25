990c8073e70fd9f507bfb98b2a817546f7cc87e6
Jennifer Kimball <156363321+jkimball3@users.noreply.github.com>
2024-09-25 10:46:00 -0400

    Pushing login shortcut files
    copy to wormington-keys folder, in there run ./lmy.sh to login (or ./lad.sh for student-admin_key)

---
+++ lad.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ssh -i student-admin_key -p 22003 student-admin@paffenroth-23.dyn.wpi.edu
+
---
+++ lmy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ssh -i group_key -p 22003 student-admin@paffenroth-23.dyn.wpi.edu
+
