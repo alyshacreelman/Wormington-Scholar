46211e78ff6115d51882fc426efb8dd6ba7aa1a8
Alysha Creelman <156394139+alyshacreelman@users.noreply.github.com>
2024-10-02 10:23:26 -0400

    Update check_health.sh

--- check_health.sh
+++ check_health.sh
@@ -2,5 +2,5 @@
 
 # Check if "app.py" is running
 if ! pgrep -f "app.py"; then
-    echo "Chatbot is down" | mail -s "Chatbot Down Alert" amcreelman@wpi.edu,eesojka@wpi.edu,jekimball@wpi.edu
+    echo "Wormington Scholar has been squashed!" | mail -s "Wormington Down Alert" amcreelman@wpi.edu,eesojka@wpi.edu,jekimball@wpi.edu
 fi
