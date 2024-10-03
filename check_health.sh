422e990a35d85dc9cd39bdfaf1ff90bd04f0f0df
Alysha Creelman <156394139+alyshacreelman@users.noreply.github.com>
2024-10-03 11:35:07 -0400

    Update check_health.sh

--- check_health.sh
+++ check_health.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Check if a file was passed as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <webhook_file>"
+    exit 1
+fi
+
+# Read the Discord webhook URL from the file passed as the first argument
+WEBHOOK_URL=$(cat "$1")
+
 # Check if "app.py" is running
 if ! pgrep -f "python.*app.py"; then
-    # Discord webhook URL
-    WEBHOOK_URL=sys.argv[1]
-
     # Message payload for Discord
     PAYLOAD='{
         "content": "Wormington Scholar has been squashed!"
