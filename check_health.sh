83bc8390497b9c6e81634f41a629fddca912b85a
Alysha Creelman <156394139+alyshacreelman@users.noreply.github.com>
2024-10-05 21:33:55 -0400

    Adding proper documentation

--- check_health.sh
+++ check_health.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Read the Discord webhook URL from the file passed as the first argument
+# Read the Discord webhook URL from the file passed as the first argument (keeping it on the computer and not pasting here for security purposes)
 WEBHOOK_URL=$(cat "$1")
 
-# The URL to check
+# We will be checking this URL which should have our product up and running
 URL="http://paffenroth-23.dyn.wpi.edu:8003/"
 
-# Check if the URL is up (HTTP status 200-299 means success)
+# Check if the URL is up
 if curl -s --head "$URL" | grep "HTTP/[1-2].[0-9] [2][0-9][0-9]" > /dev/null; then
     # Message payload for Discord (healthy)
     PAYLOAD=$(cat <<EOF
@@ -28,4 +28,5 @@ fi
 # Send notification to Discord using the webhook
 curl -H "Content-Type: application/json" -d "$PAYLOAD" "$WEBHOOK_URL"
 
+# Run the key setup file to get Wormington back up and running
 ./Wormington-Scholar/key_setup.sh
