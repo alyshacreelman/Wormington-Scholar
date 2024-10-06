8f9989e06b61f24109e8a7bbb470980ddf9beee5
Alysha Creelman <156394139+alyshacreelman@users.noreply.github.com>
2024-10-05 21:34:42 -0400

    Documentation

--- key_gen.sh
+++ key_gen.sh
@@ -1,13 +1,7 @@
 #! /bin/bash
 
-# move to tmp
+# move to tmp (used an absolute path so every group member could get there)
 cd /tmp
 
-# generate the key
+# generate the key group_key which we will need to create a password for
 ssh-keygen -f group_key -t ed25519
-
-
-
-
-
-
