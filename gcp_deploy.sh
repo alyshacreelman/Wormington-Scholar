10848ad34d0796002d2b92a8674ec7f204b5f85c
Alysha Creelman <156394139+alyshacreelman@users.noreply.github.com>
2024-11-25 15:53:05 -0500

    Updating the GCP one

--- gcp_deploy.sh
+++ gcp_deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud run deploy wormington-scholar-app --image alyshacreelman/wormington-scholar-cs4 --port 7860
+gcloud run deploy wormington-scholar-app --image alyshacreelman/wormington-cs4 --port 7860
